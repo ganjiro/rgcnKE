3dccbcb1cce852c760e97d3cd4e47675c32dcd33
mistretta_marco <marco.mistretta@stud.unifi.it>
2022-01-25 15:45:54 +0100

    sparql endpoint

--- fuseki_endpoint.py
+++ fuseki_endpoint.py
@@ -1,6 +1,7 @@
 
 from SPARQLWrapper import SPARQLWrapper, JSON
 
+# 3 tipo (TDB2)
 sparql = SPARQLWrapper("http://localhost:3030/myFuseki/query")
 
 # query = "select distinct ?p ?o from <http://www.disit.org/km4city/resource/Eventi/Eventi_a_Firenze> where {service <https://servicemap.disit.org/WebAppGrafo/sparql> {<http://www.disit.org/km4city/schema#Event> ?p ?o.}}"
