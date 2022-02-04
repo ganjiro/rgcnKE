c21f06777863c23e6d33181f81118fdbe0a50ed5
mistretta_marco <marco.mistretta@stud.unifi.it>
2022-02-04 16:03:33 +0100

    to do check

--- main_node_classification.py
+++ main_node_classification.py
@@ -12,12 +12,12 @@ curr_dir = str(os.getcwd()) + "\\km4city\\"
 data = ll.load_dataset(label_header='label', nodes_header='nodes', datasets=datasets, dir=curr_dir)
 
 # parametri
-n_hidden = 20
+n_hidden = 16
 num_hidden_layers = 1
 epochs = 10000
-l2norm = 5e-4
-lr = 0.04
-num_bases = 16
+l2norm = 0.0005
+lr = 0.01
+num_bases = 28
 
 model = Model(data=data, h_dim=n_hidden, num_hidden_layers=num_hidden_layers, n_epochs=epochs, l2norm=l2norm,
               lr=lr, num_bases=num_bases)
