b8355ee5448d03b9693dc01479da3198cd74a3ea
mistretta_marco <marco.mistretta@stud.unifi.it>
2022-02-07 21:06:01 +0100

    set up comparison with MINDWALC

--- compare1.py
+++ compare1.py
@@ -2,21 +2,45 @@ import rdflib
 import pandas as pd
 from sklearn.metrics import accuracy_score
 
+import numpy as np
+
+# labels = np.load("km4city/labels.npz")
+# labels = labels["indices"]
+
+
 from tree_builder import MINDWALCTree, MINDWALCForest, MINDWALCTransform
 from datastructures import Graph
 
+
+rdf_file = 'km4city/km4c_stripped.nt'
+_format = 'nt'
+train_file = 'km4city/trainingSet.tsv'
+test_file = 'km4city/testSet.tsv'
+entity_col = 'nodes'
+label_col = 'label'
+label_predicates = [
+    rdflib.URIRef('http://www.disit.org/km4city/schema#eventCategory')
+]
+output = 'mio_km4city_compare.p'
+
+print(end='Loading data... ', flush=True)
 g = rdflib.Graph()
-g.parse('data/AIFB/aifb.n3', format='n3')
+g.parse(rdf_file, format=_format)
+print('OK')
 
-train_data = pd.read_csv('data/AIFB/AIFB_train.tsv', sep='\t')
-train_entities = [rdflib.URIRef(x) for x in train_data['person']]
-train_labels = train_data['label_affiliation']
+test_data = pd.read_csv(train_file, sep='\t')
+train_data = pd.read_csv(test_file, sep='\t')
 
-test_data = pd.read_csv('data/AIFB/AIFB_test.tsv', sep='\t')
-test_entities = [rdflib.URIRef(x) for x in test_data['person']]
-test_labels = test_data['label_affiliation']
+train_entities = [rdflib.URIRef(x) for x in train_data[entity_col]]
+train_labels = train_data[label_col]
+
+test_entities = [rdflib.URIRef(x) for x in test_data[entity_col]]
+test_labels = test_data[label_col]
 
 kg = Graph.rdflib_to_graph(g, label_predicates=label_predicates)
+
+
+#à###########hediwuwhduw
 
 clf = MINDWALCTree()
 #clf = MINDWALCForest()
