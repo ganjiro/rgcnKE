2010a1f9909cce4f8f926963736f68519eb445ae
Marco Mistretta <49488524+marcomistretta@users.noreply.github.com>
2022-02-19 14:20:42 +0100

    rearrange project structure pt1

--- manage_dataset/splidataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from sklearn.model_selection import train_test_split
-
-df = pd.read_csv("km4city_test/daSplittare.tsv", sep='\t', encoding='ISO-8859-1')
-train, test = train_test_split(df, test_size=0.2)
-
-train.to_csv('km4city_test/testSet.tsv', index=False, sep='\t')
-test.to_csv('km4city_test/trainingSet.tsv', index=False, sep='\t')
-
