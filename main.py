1a31bcb58cda88b28d0b8f71070bcb4bc768b388
ganjiro <52581772+ganjiro@users.noreply.github.com>
2022-03-09 11:50:34 +0100

    pre release

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import os
-
+import time
+from src.DataManagement.reformat_data import reformat_data
+from src.DataManagement.download_raw_dataset import download_km4c
+from src.mindwalc import MINDWALC_node_classification
+from src.noge import NoGE_link_prediction
 from src.rgcn import RGCN_node_classification
+from src.testpykeen import pykeen_link_prediction
 
 if __name__ == '__main__':
     path_to_dataset = r'{}\dataset'.format(os.getcwd())
@@ -18,9 +23,13 @@ if __name__ == '__main__':
     # dq_gnn = NoGE_link_prediction(path_to_dataset)
     # dq_gnn.fit()
 
-    # decision_tree = MINDWALC_node_classification(path_to_dataset)
-    # decision_tree.fit()
-    # decision_tree.predict()
+    start = time.time()
+    decision_tree = MINDWALC_node_classification(path_to_dataset)
+    decision_tree.fit()
+    decision_tree.predict()
+    end = time.time()
 
     # tuckER = pykeen_link_prediction(path_to_dataset)
     # tuckER.fit()
+
+    print("Time elaplesed ",end - start)
